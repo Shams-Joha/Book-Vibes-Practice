6e108592a5ad0dbe0639f95ac9162f18651f26a0
Shams Joha <shamsjoha3@gmail.com>
2024-11-02 14:36:10 +0600

    ratings and badges

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,7 +1,7 @@
 
 
 const Book = ({ book }) => {
-    const { image, bookName, author } = book;
+    const { image, bookName, author, tags, category } = book;
     return (
         <div className="card bg-base-100 w-96 shadow-xl p-6">
             <figure className="bg-gray-200 py-8 rounded-2xl">
@@ -11,14 +11,26 @@ const Book = ({ book }) => {
                     alt={bookName} />
             </figure>
             <div className="card-body">
+                <div className="flex justify-center gap-3">
+                    {
+                        tags.map(tag => <button className="btn btn-xs bg-green-200 text-green-500">{tag}</button>)
+                    }
+                </div>
+
                 <h2 className="card-title">
                     {bookName}
                     <div className="badge badge-secondary">NEW</div>
                 </h2>
                 <p>{author}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                <div className="border-t-2 border-dashed"></div>
+                <div className="card-actions justify-between">
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="rating">
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-100" />
+                    </div>
                 </div>
             </div>
         </div>
