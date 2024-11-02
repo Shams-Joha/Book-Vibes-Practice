b3d391a34bb3fb5beb1b87c3cbe3334ba4b44abf
Shams Joha <shamsjoha3@gmail.com>
2024-11-02 16:58:34 +0600

    save markasread to local storage

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../../utility/addtoDB";
 
 
 const BookDetails = () => {
@@ -6,19 +7,29 @@ const BookDetails = () => {
     const data = useLoaderData();
     const id = parseInt(bookId);
 
+    
+
     const book = data.find(book => book.bookId === id);
     // you got the full book object now destructure.
     // Rename duplicate
     const { bookId: currentBookId, image } = book;
 
+    const handleMarkAsRead = (id) => {
+        // Understand what to store: for my personal BookId
+        // Where to store: Database. Arrary, list or collection
+        // Check if the book is already in the list. if not add, if yes don't.
+        // 
+        addToStoredReadList(id);
+
+    }
 
     return (
         <div className="my-12">
             <h2>Book Details {currentBookId}</h2>
             <img className="w-36" src={image} alt="" />
             <br></br>
-            <button className="btn mr-4 btn-accent">Read</button>
-            <button className="btn btn-accent">Wishlist</button>
+            <button onClick={() => { handleMarkAsRead(bookId) }} className="btn mr-4 btn-accent">Read</button>
+            <button className="btn btn-accent">Add to Wishlist</button>
         </div>
         // Do the customization yourself.
 
