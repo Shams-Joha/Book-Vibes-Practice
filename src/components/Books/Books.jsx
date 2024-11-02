1ed95b6ad941bfc34aece315c3d5db9d0fbe673a
Shams Joha <shamsjoha3@gmail.com>
2024-11-02 13:41:39 +0600

    book rendering and responsive

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import Book from "../Book/Book";
 
 const Books = () => {
     const [books, setBooks] = useState([]);
@@ -12,7 +12,12 @@ const Books = () => {
     return (
         <div>
             <h2 className="text-4xl text-center font-bold">Books</h2>
-            <p>{books.length}</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {
+                    books.map(book => <Book key={book.bookId} book={book}></Book>)
+                }
+            </div>
+
         </div>
     );
 };
